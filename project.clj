c701d1f1f047441ab262a9cd7cb35b042b1303ee
Chetan Padia <chetan@swiftkey.net>
2013-01-31 11:30:11 +0000

    switch back to stable leiningen version

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject certificates "0.1.0-SNAPSHOT"
   :description "First Annual Little Printer Awards"
   :url "http://sarahrules.co.uk/falpa"
-  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [ring/ring-core "1.1.8"]
